4e5e7344d28c0f3878069395eee5434602830a5a
Denis Carriere <carriere.denis@gmail.com>
2026-03-29 15:15:58 -0400

    remove version

--- svm-transfers/clickhouse/schema.0.functions.helpers.sql
+++ svm-transfers/clickhouse/schema.0.functions.helpers.sql
@@ -17,6 +17,3 @@ CREATE OR REPLACE FUNCTION string_to_uint8 AS (raw) ->
 -- Returns NULL if the input is empty or NULL
 CREATE OR REPLACE FUNCTION string_or_null AS (raw) ->
     accurateCastOrNull(nullIf(trimBoth(raw), ''), 'String');
-
-CREATE OR REPLACE FUNCTION to_version AS (block_num, transaction_index, instruction_index) ->
-    block_num * 1e6 + transaction_index * 1e3 + instruction_index;
