d71b7f159456f14ab4440b9610e05c9876a14661
Denis Carriere <carriere.denis@gmail.com>
2026-03-30 16:22:14 -0400

    remove extra clickhouse sQL requirements

--- svm-dex/clickhouse/schema.1.events.heaven.sql
+++
@@ -1,7 +0,0 @@
--- Heaven Sell --
-CREATE TABLE IF NOT EXISTS heaven_sell AS BASE_EVENTS
-COMMENT 'Heaven Sell';
-ALTER TABLE heaven_sell
-    ADD COLUMN IF NOT EXISTS user               String COMMENT 'User account',
-    ADD COLUMN IF NOT EXISTS mint               String COMMENT 'Token mint',
-    ADD COLUMN IF NOT EXISTS amount             UInt64 COMMENT 'Amount';
--- svm-dex/src/lib.rs
+++ svm-dex/src/lib.rs
@@ -4,44 +4,22 @@ use substreams::{errors::Error, pb::substreams::Clock};
 use substreams_database_change::pb::sf::substreams::sink::database::v1::DatabaseChanges;
 use substreams_solana::base58;
 
-fn protocol_slug(protocol: i32) -> &'static str {
-    match pb::Protocol::try_from(protocol).unwrap_or(pb::Protocol::Unspecified) {
-        pb::Protocol::Unspecified => "protocol_unspecified",
-        pb::Protocol::Boop => "boop",
-        pb::Protocol::Darklake => "darklake",
-        pb::Protocol::Dumpfun => "dumpfun",
-        pb::Protocol::JupiterV4 => "jupiter_v4",
-        pb::Protocol::JupiterV6 => "jupiter_v6",
-        pb::Protocol::MeteoraDaam => "meteora_daam",
-        pb::Protocol::MeteoraDllm => "meteora_dllm",
-        pb::Protocol::OrcaWhirlpool => "orca_whirlpool",
-        pb::Protocol::Pumpfun => "pumpfun",
-        pb::Protocol::PumpfunAmm => "pumpfun_amm",
-        pb::Protocol::RaydiumAmmV4 => "raydium_amm_v4",
-        pb::Protocol::RaydiumClmm => "raydium_clmm",
-        pb::Protocol::RaydiumCpmm => "raydium_cpmm",
-        pb::Protocol::RaydiumLaunchpad => "raydium_launchpad",
-    }
-}
-
 #[substreams::handlers::map]
-pub fn db_out(mut clock: Clock, dex_swaps_events: pb::Events) -> Result<DatabaseChanges, Error> {
+pub fn db_out(mut clock: Clock, swaps: pb::Events) -> Result<DatabaseChanges, Error> {
     let mut tables = substreams_database_change::tables::Tables::new();
 
-    for (transaction_index, transaction) in dex_swaps_events.transactions.iter().enumerate() {
-        for (swap_index, swap) in transaction.swaps.iter().enumerate() {
-            let key = common_key_v2(&clock, transaction_index, swap_index);
+    for (transaction_index, transaction) in swaps.transactions.iter().enumerate() {
+        for (instruction_index, swap) in transaction.swaps.iter().enumerate() {
+            let key: [(&str, String); 3] = common_key_v2(&clock, transaction_index, instruction_index);
+            let signers_raw = transaction.signers.iter().map(base58::encode).collect::<Vec<_>>().join(",");
             let row = tables
                 .create_row("swaps", key)
                 .set("signature", base58::encode(&transaction.signature))
                 .set("fee_payer", base58::encode(&transaction.fee_payer))
-                .set(
-                    "signers_raw",
-                    transaction.signers.iter().map(base58::encode).collect::<Vec<_>>().join(","),
-                )
+                .set("signers_raw", signers_raw)
                 .set("fee", transaction.fee)
                 .set("compute_units_consumed", transaction.compute_units_consumed)
-                .set("protocol", protocol_slug(swap.protocol))
+                .set("protocol", swap.protocol.to_string())
                 .set("amm", base58::encode(&swap.amm))
                 .set("amm_pool", base58::encode(&swap.amm_pool))
                 .set("user", base58::encode(&swap.user))
